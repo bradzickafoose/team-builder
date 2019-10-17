d82f22ed73adf6ca0079f7f294f2a56cce77a115
Brad <brad@viziworx.com>
2019-10-17 16:31:18 -0400

    Setup Link and Route for Home and Add Person

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import Card from './components/Card';
 import Form from './components/Form';
+import { Route, Link } from 'react-router-dom';
 
 function App() {
 	const [ people, setPeople ] = useState([
@@ -12,10 +13,15 @@ function App() {
 	const addPerson = (person) => {
 		setPeople([ ...people, { ...person, id: Date.now() } ]);
 	};
+
+	const editPerson = (editedPerson) => {};
+
 	return (
 		<div className='App'>
-			<Form submitPerson={addPerson} buttonText='Add Person' />
-			{people.map((person, index) => <Card key={index} person={person} />)}
+			<Link to='/'>Home</Link>
+			<Link to='/add'>Add Person</Link>
+			<Route path='/add' render={(props) => <Form submitPerson={addPerson} buttonText='Add Person' />} />
+			<Route exact path='/' render={(props) => people.map((person, index) => <Card key={index} person={person} />)} />
 		</div>
 	);
 }
