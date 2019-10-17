1a1d45cd8a9a430f05992f26783cf436a61af752
Brad <brad@viziworx.com>
2019-10-17 16:07:45 -0400

    Finished form build and mvp

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,19 +1,21 @@
 import React, { useState } from 'react';
 import './App.css';
-import Form from './components/Form;';
+import Card from './components/Card';
+import Form from './components/Form';
 
 function App() {
 	const [ people, setPeople ] = useState([
 		{ id: 0, name: 'Bradley', email: 'stop@hammer.time', role: 'student' },
-		{ id: 1, name: 'Justin', email: 'justin@reactjs.io', role: 'TL' },
+		{ id: 1, name: 'Justin', email: 'justin@gmail.com', role: 'TL' },
 	]);
 
 	const addPerson = (person) => {
-		setPeople([ ...people, { person, id: Date.now() } ]);
+		setPeople([ ...people, { ...person, id: Date.now() } ]);
 	};
 	return (
 		<div className='App'>
-			<Form addPerson={addPerson} />
+			<Form submitPerson={addPerson} buttonText='Add Person' />
+			{people.map((person, index) => <Card key={index} person={person} />)}
 		</div>
 	);
 }
--- team-builder/src/components/Card.js
+++ team-builder/src/components/Card.js
@@ -1,1 +1,12 @@
 import React from 'react';
+
+export default function Card(props) {
+	const { person } = props;
+	return (
+		<div>
+			<div>{person.name}</div>
+			<div>{person.email}</div>
+			<div>{person.role}</div>
+		</div>
+	);
+}
